67c42775e8f843fa436bf75dabb6c4843fc03d37
yqxshiki <yiqixing@yeah.net>
2020-03-20 19:50:07 +0800

    el_button

---
+++ src/components/Elbutton.vue
@@ -0,0 +1,57 @@
+<template>
+  <button
+    class="el-button"
+    :disabled="disabled"
+    :class="[
+    type ? 'el-button--'+type : ' ',
+    {
+      'is-plain':plain,
+      'is-round':round,
+      'is-circle':circle,
+      'is-disabled':disabled,
+      'is-loading':loading
+    }
+  ]"
+    :type="nativeType"
+    @click="handClick"
+  >
+    <i class="el-icon-loading" v-if="loading"></i>
+    <i :class="icon" v-if="icon || !loading"></i>
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "elbutton",
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    },
+    nativeType: {
+      type: String,
+      default: "button"
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    disabled: Boolean,
+    plain: Boolean,
+    round: Boolean,
+    circle: Boolean,
+    disabled: Boolean,
+    loading: Boolean
+  },
+  methods: {
+    handClick() {
+      console.log("handClick");
+    }
+  }
+};
+</script>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+import elbutton from './components/Elbutton.vue'
+
+Vue.component("el-button", elbutton)
 
 Vue.config.productionTip = false
 
